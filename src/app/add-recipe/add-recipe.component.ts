704ac08a504555032b42112977d4f7b89459df5b
Sukhpreet-s <sukhpreets6718@gmail.com>
2021-03-25 12:07:27 -0400

    update : changed css loader from stylus to scss

--- src/app/add-recipe/add-recipe.component.ts
+++ src/app/add-recipe/add-recipe.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-add-recipe',
   templateUrl: './add-recipe.component.html',
-  styleUrls: ['./add-recipe.component.styl']
+  styleUrls: ['./add-recipe.component.scss']
 })
 export class AddRecipeComponent implements OnInit {
 
