dec3d55c7b7250f8598966120fd18f094d970858
sukhpreet-s <sukhpreets6718@gmail.com>
2021-04-14 22:24:04 -0400

    update : display error if no ingredient is added and also if trying to submit empty form

--- src/app/components/add-recipe/add-recipe.component.ts
+++ src/app/components/add-recipe/add-recipe.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router"
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Ingredient } from 'models/Ingredient';
 import { BackendService } from 'services/backend/backend.service';
 
@@ -30,8 +30,11 @@ export class AddRecipeComponent implements OnInit {
   ngOnInit(): void {}
 
   // Events
+
+  /**
+   * Adds the ingredient name and quantity to ingredient array.
+   */
   addIngredient(): void {
-    // Validate values 
 
     const newIngredientGroup = new FormGroup({
       name: new FormControl({ value: this.ingredientName?.value, disabled: true }),
@@ -43,21 +46,28 @@ export class AddRecipeComponent implements OnInit {
     this.ingredientQuantity?.reset();
   }
 
+  /**
+   * Remove the ingredient from array using the array index.
+   * @param index array index
+   */
   removeIngredient(index: number): void {
     this.ingredients.removeAt(index);
   }
 
+  /**
+   * Submits the form data.
+   * Also, marks the fields as touched so that errors are displayed if trying to submit empty form. 
+   */
   handleSubmit(): void {
-    if (this.recipeForm.valid) {
-      console.log(this.recipeForm.value)
-      
+    this.markFormGroupTouched(this.recipeForm);
 
+    if (this.recipeForm.valid) {
       const { dishName, instructions } = this.recipeForm.value;
       const ingredients: Ingredient[] = this.recipeForm.controls.ingredients.value;
   
       this.backendService.createRecipe({ dishName, ingredients, recipeInstruction: instructions })
         .subscribe(() => this.router.navigate(['/list-recipes']));
-    }
+    } 
       
   }
 
@@ -68,5 +78,16 @@ export class AddRecipeComponent implements OnInit {
   get ingredients(): FormArray { return <FormArray> this.recipeForm.get('ingredients');}
   get instructions() { return this.recipeForm.get('instructions'); }
 
+
+  /**
+   * Marks all controls in a form group as touched
+   * Ref: https://stackoverflow.com/questions/40529817/reactive-forms-mark-fields-as-touched
+   * @param formGroup - The form group to touch
+   */
+   private markFormGroupTouched(formGroup: FormGroup) {
+    (<any>Object).values(formGroup.controls).forEach((control: AbstractControl) => {
+      control.markAsTouched();
+    });
+  }
     
 }
